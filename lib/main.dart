197a0ecaf754da53364d4c1af6e2ec813c28a62d
getkeshav <20bcs111@nith.ac.in>
2023-04-26 23:04:52 +0530

    Day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -8,22 +8,25 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override                           // build fn is over written again and again to implement the changes
-  Widget build(BuildContext context) { //context specify location of each widget in widget tree
+  @override // build fn is over written again and again to implement the changes
+  Widget build(BuildContext context) {
+    //context specify location of each widget in widget tree
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
-        ),
-         initialRoute: "/login", //this is the 1st route we want to show on screen when app opens
-        routes: {
-          "/":(context) => LoginPage(), //by default initial route is /
-          "/home":(context) => HomePage(),
-          "/login": (context) =>LoginPage(),
-        },
+      ),
+      initialRoute:
+          "/login", //this is the 1st route we want to show on screen when app opens
+      routes: {
+        "/": (context) => LoginPage(), //by default initial route is /
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,15 +4,57 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
+        color: Colors.white,
         child: Column(
-      //column has vertical as main axis
-      children: [
-        Image.asset(
-          "assets/images/login_image.png",
-          fit: BoxFit.cover, //covers the entire target box cutting the image as less as possible
-        ),
-      ],
-    ));
+          //column has vertical as main axis
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit
+                  .cover, //covers the entire target box cutting the image as less as possible
+            ),
+            // SizedBox(
+            //   height: 20,
+            // ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    child: Text("Login"),
+                    onPressed: () {
+                      print("Hi Keshav");
+                    },
+                  ),
+                )
+              ]),
+            ),
+          ],
+        ));
   }
 }
