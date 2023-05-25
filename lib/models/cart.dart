5aff4a5bb82dc2e8d83f1d356c4996b94a896a0d
getkeshav <20bcs111@nith.ac.in>
2023-05-25 16:34:25 +0530

    Day 23 completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_application_1/models/catalog.dart';
+
+class CartModel{
+ late CatalogModel _catalog;
+  // Collection of IDs - store Ids of each item
+  final List<int> _itemIds=[];
+
+  //Get Catalog
+  CatalogModel get catalog=>_catalog;
+  //Set Catalog
+  set catalog(CatalogModel newCatalog){
+    assert(newCatalog !=null);
+    _catalog=newCatalog;
+  }
+  //Get items in the cart
+  List<Item>get items=>_itemIds.map((id)=>_catalog.getById(id)).toList(); // we'll get all the items in the cart from here
+
+  num get totalPrice =>items.fold(0, (total, current) => total+current.price);
+
+  //Add item
+  void add(Item item)
+  {
+    _itemIds.add(item.id);
+  }
+
+  //Remove Item
+  void remove(Item item)
+  {
+    _itemIds.remove(item.id);
+  }
+}
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -33,7 +33,13 @@ class _CartTotal extends StatelessWidget {
           "\$9999".text.xl5.color(context.accentColor).make(),
           30.widthBox,
           ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: "Buying not Supported yet".text.make(),
+                      ),
+                    );
+                  },
                   style: ButtonStyle(
                     backgroundColor:
                         MaterialStateProperty.all(context.theme.buttonColor),
